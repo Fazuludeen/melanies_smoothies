fcf181f2a8ecfc044bd5b7b69a5241b7f12c3742
Fazuludeen <fazuludeen.j@gmail.com>
2024-09-02 01:29:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
+st.title('My Parents New Healthy Diner')
  
 # Write directly to the app
 st.title(":apple: Customize your Smoothie :apple:")
@@ -12,20 +13,33 @@ st.write(
 name_on_order = st.text_input('Name of Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
  
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+cnx=st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
  
+#Convert the snowflake dataframe to Pandas dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
  
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:'
     ,my_dataframe
+    ,max_selections=5
 )
  
 if  ingredients_list:
      ingredients_string=''
-     for fruit_choosen in ingredients_list:
-         ingredients_string += fruit_choosen + ' '
+     for fruit_chosen in ingredients_list:
+         ingredients_string += fruit_chosen + ' '
+ 
+         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+         st.subheader(fruit_chosen + ' Nutrition Information')
+         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     #st.write(ingredients_string)  
 
  
